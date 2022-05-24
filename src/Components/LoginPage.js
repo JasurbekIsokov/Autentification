98c83408b5d6ab0a40089b97b42ae5b385d7944e
Jasurbek <jasurisoqov685@gmail.com>
2022-05-24 23:30:42 +0500

    create autentitifation axios post

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,8 +1,43 @@
+import { useState } from "react";
+import axios from "axios";
+
 const Demo = () => {
+  const [login, setLogin] = useState("");
+  const [pass, setPass] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // console.log(login, pass);
+
+    try {
+      const { data } = await axios.post(
+        "https://pwcontrol.herokuapp.com/api/users/login",
+        {
+          userName: login,
+          password: pass,
+        }
+      );
+      // console.log(data);
+    } catch (error) {
+      alert(error.massage);
+    }
+  };
+
   return (
-    <form className="login_form">
-      <input className="login" type={"text"} placeholder="Login..." />
-      <input className="pass" type={"password"} placeholder="Password" />
+    <form className="login_form" onSubmit={handleSubmit}>
+      <input
+        className="login"
+        type={"text"}
+        placeholder="Login..."
+        onChange={(e) => setLogin(e.target.value)}
+      />
+      <input
+        className="pass"
+        type={"password"}
+        placeholder="Password"
+        onChange={(e) => setPass(e.target.value)}
+      />
       <button className="submit_btn" type="submit">
         Submit
       </button>
